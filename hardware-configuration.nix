9ff193922165ae3b7ecca00895263d126d451172
Robert Cina <rob.cina@gmail.com>
2023-09-15 15:57:52 -0400

    Fix filesystem labels

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -14,11 +14,13 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/5af05cc1-c848-4dd8-b358-43033ba988dc";
-      fsType = "ext4";
+    { #device = "/dev/disk/by-uuid/5af05cc1-c848-4dd8-b358-43033ba988dc";
+      device = "/dev/disk/by-label/nixos";
+       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."luks-d7ac53da-43ab-4b6a-ae80-43e8c866e159".device = "/dev/disk/by-uuid/d7ac53da-43ab-4b6a-ae80-43e8c866e159";
+  #boot.initrd.luks.devices."luks-d7ac53da-43ab-4b6a-ae80-43e8c866e159".device = "/dev/disk/by-uuid/d7ac53da-43ab-4b6a-ae80-43e8c866e159";
+   boot.initrd.luks.devices."cryptroot".device = "/dev/nvme0n1p5";
 
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/2020-1FEA";
