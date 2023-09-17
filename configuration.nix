9bc9230dee0c7911b15404511c1fcd109673e467
Robert Cina <rob.cina@gmail.com>
2023-09-17 12:51:46 -0400

    Change boot configurationLimit

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.systemd-boot.configurationLimit = 15;
 
   # Setup keyfile
   boot.initrd.secrets = {
